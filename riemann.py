a3579a6fe6baaad059471170e26e8033b55620db
rileybrubaker <rileybrubaker@ufl.edu>
2025-02-19 19:31:51 -0500

    left_riemann_sum

--- riemann.py
+++ riemann.py
@@ -2,14 +2,12 @@
 import numpy as np
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
     #Make x_vals a sorted array
-    x_vals = np.sort(x_vals)
+    #x_vals = np.sort(x_vals)
     #Interval was given as [x_vals[0], x_vals[-1]]
     a, b = x_vals[0], x_vals[-1]
-    #Find the width of each subinterval
+    #Find the width of each sub-interval
     n = len(x_vals) - 1
     delta_x = (b - a) / n
     # Calculate left-endpoint sum
-    left_sum = 0
-    for i in range(n):
-        left_sum += func(x_vals[i]) * delta_x
+    left_sum = np.sum(func(x_vals[n]) * delta_x)
     return left_sum
