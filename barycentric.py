c5af3c53d3c6ec9777f9714ae1ce11f044090c64
sophialiu727 <sophiayuhanliu@gmail.com>
2025-02-20 21:15:09 -0500

    get_barycentric_coordinates method

--- barycentric.py
+++ barycentric.py
@@ -1,11 +1,37 @@
 import numpy as np
 # import matplotlib_inline
 #I forgot to write in my comment, I also created the file barycentric too
-def get_barycentric_coordinates():
-    pass
+
+def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
+
+    #extracting the x and y coordinates from the 2x3 array
+    x1, x2, x3 = triangle_coordinates[0]
+    y1, y2, y3 = triangle_coordinates[1]
+
+    #extracting the cartesian coordinates as x and y
+    x, y = point_coordinates
+
+    #constructing matrix that has the x and y coordinates of the vertices
+    #adding row of 1's to ensure the barycentric coordinates add up to 1, which is a rule stated above
+    b = np.array([
+        [x1, x2, x3],
+        [y1, y2, y3],
+        [1, 1, 1]
+    ])
+
+    #added 1 to vector to maintain 3x1 and consistency with b matrix
+    a = np.array(
+        [x, y, 1]
+    )
+
+    #calculates barycentric as 1D array
+    v = np.linalg.solve(b, a)
+
+    return v
+
 def get_cartesian_coordinates():
     pass
-import numpy as np
+
 def is_inside_triangle(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray  ):
 
     x, y = point_coordinates
